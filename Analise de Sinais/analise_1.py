ab655258869151cda884fc4b6a49854450abfa73
Leonardo Kewitz <holland9@gmail.com>
2014-04-22 23:17:35 -0300

    Correções em 'Analise de Sinais/analise_1.py'.

--- Analise de Sinais/analise_1.py
+++ Analise de Sinais/analise_1.py
@@ -9,11 +9,11 @@ import numpy as np
 
 
 #Define time
-t = arange(-50,50,dtype=float64)
+t = np.arange(-50,50,.1,dtype=np.float64)
 
 #Define functions
 ut = lambda t: 1 if t >= 0 else 0
-xt = lambda t: 2*t*sin(3*t - 2)*ut(t)
+xt = lambda t: 2*t*np.sin(3*t - 2)*ut(t)
 
 #Define signals
 x1 = [xt(a) for a in t]
@@ -23,8 +23,8 @@ od = [(xt(a)-xt(-a))/2 for a in t]
 #Plot x(t)
 lx1, = plt.plot(t, x1)
 #Plot Even and Odd components
-lev, = plt.plot(t, ev)
-lod, = plt.plot(t, od)
+lev, = plt.plot(t, ev, 'r-', alpha=0.6)
+lod, = plt.plot(t, od, 'g-', alpha=0.6)
 
 #Add legend and stuff...
 plt.legend( (lx1, lev, lod), (r'$x(t)$',r'$Ev\{x(t)\}$', r'$Od\{x(t)\}$'), 'best', shadow=True)
