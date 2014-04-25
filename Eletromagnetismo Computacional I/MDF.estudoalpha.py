584f17a6fbc7974bddfaa6c13bf4b6c9138f58ea
Leonardo Kewitz <holland9@gmail.com>
2014-04-25 18:30:17 -0300

    MDF.py funciona somente como biblioteca. Separado os exercicios e simulações.

---
+++ Eletromagnetismo Computacional I/MDF.estudoalpha.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 25 17:09:45 2014
+
+@author: leo
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import MDF as m
+import multiprocessing
+
+threads = multiprocessing.cpu_count()
+
+def f(alpha):
+    test = m.MDF(np.arange(0,10), np.arange(0,10))
+    test.delta = 1E-8
+    test.alpha = np.float64(alpha)
+    test.bounds['left'] = ['n' for a in test.bounds['left']]
+    test.bounds['right'] = ['n' for a in test.bounds['right']]
+    test.bounds['top'] += 100
+    test.run()
+    print "Alpha %.2f -> %i iterations." % (alpha, test.k)
+    return test.k
+    
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    alphas = np.arange(1,2,.01)
+    pool = multiprocessing.Pool(processes=threads)    # start 4 worker processes
+    x = pool.map(f, alphas)          # prints "[0, 1, 4,..., 81]"
+    bestA, bestK = (alphas[x.index(min(x))],min(x))
+    plt.plot(alphas, x)
+    plt.xlabel(r'$\alpha$')
+    plt.ylabel('Iterations')
+    plt.title(r'$\alpha$ impact on performance.')    
+    plt.annotate(r'$\alpha=$'+str(bestA), xy=(bestA, bestK),  xycoords='data',
+                xytext=(30, 30), textcoords='offset points',
+                arrowprops=dict(arrowstyle="->",
+                                connectionstyle="arc3,rad=.2")
+                )
---
+++ Eletromagnetismo Computacional I/exerciciosMDFmagnetostatica.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 25 16:17:46 2014
+
+@author: leo
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import MDF as m
+
+
+plt.subplot(2,2,1)
+ex1 = m.MDF(np.arange(0,10,.5), np.arange(0,10,.5))
+ex1.bounds['left'] = ['n' for a in ex1.bounds['left']]
+ex1.bounds['right'] = ['n' for a in ex1.bounds['right']]
+ex1.bounds['top'] += 100
+ex1.run()
+ex1.plot()
+plt.title("a)")
+
+plt.subplot(2,2,2)
+ex2 = m.MDF(np.arange(0,10,.5), np.arange(0,10,.5))
+ex2.alpha = 1.1
+ex2.bounds['top'] += 100
+ex2.bounds['top'][0:1] = 0
+ex2.bounds['top'][19:20] = 0
+ex2.run()
+ex2.plot()
+plt.title("b)")
+
+plt.subplot(2,2,3)
+ex3 = m.MDF(np.arange(0,10,.3), np.arange(0,10,.3))
+ex3.bounds['top'] = ['n']+[100 for a in range(len(ex3._x))]+['n']
+ex3.bounds['left'] = ['n']+[60 for a in range(len(ex3._y))]+['n']
+ex3.bounds['bot'] = ['n']+[40 for a in range(len(ex3._x))]+['n']
+ex3.bounds['right'] = ['n']+[20 for a in range(len(ex3._y))]+['n']
+ex3.run()
+ex3.plot()
+plt.title("c)")
