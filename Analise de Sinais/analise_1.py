300642f11a06ca33d4584a5a8ade808400bc3b53
Leonardo Kewitz <holland9@gmail.com>
2014-04-25 18:27:44 -0300

    MDF com suporte a Alpha.

--- Analise de Sinais/analise_1.py
+++ Analise de Sinais/analise_1.py
@@ -20,15 +20,23 @@ x1 = [xt(a) for a in t]
 ev = [(xt(a)+xt(-a))/2 for a in t]
 od = [(xt(a)-xt(-a))/2 for a in t]
 
+#Setup figure.
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
 #Plot x(t)
-lx1, = plt.plot(t, x1)
+
+lx1, = ax1.plot(t, x1)
+
 #Plot Even and Odd components
-lev, = plt.plot(t, ev, 'r-', alpha=0.6)
-lod, = plt.plot(t, od, 'g-', alpha=0.6)
+lev, = ax2.plot(t, ev, 'r-', alpha=0.6)
+lod, = ax3.plot(t, od, 'g-', alpha=0.6)
 
 #Add legend and stuff...
-plt.legend( (lx1, lev, lod), (r'$x(t)$',r'$Ev\{x(t)\}$', r'$Od\{x(t)\}$'), 'best', shadow=True)
-plt.title(r"$x(t) = 2 t \sin(3t-2) u(t)$")
+ax1.set_title(r"$x(t) = 2 t \sin(3t-2) u(t)$")
+ax2.set_title(r"$Ev\{x(t)\}$")
+ax3.set_title(r"$Od\{x(t)\}$")
 plt.xlabel(r"$t$")
-plt.grid(True)
+
+ax1.grid(True)
+ax2.grid(True)
+ax3.grid(True)
 plt.show()
--- Eletromagnetismo Computacional I/MDF.py
+++ Eletromagnetismo Computacional I/MDF.py
@@ -6,31 +6,28 @@ Created on Wed Apr 16 15:00:06 2014
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 class MDF:
     """Uma classe de cálculo do Método das Diferenças Finitas."""
-    alpha = 1
     def __init__(self,x,y):
         self._x = x
         self._y = y
         self.k = 0
-        self.delta = 0.0001   #Max value delta for convergence
+        self.verb = False
+        self.alpha = 1.0
+        self.delta = 1E-8   #Max value delta for convergence
         self.X, self.Y = np.meshgrid(x, y)
-        self.space = np.zeros(self.X.shape)
+        self.space = np.zeros(self.X.shape, dtype=np.float64)
         self.bounds = {'top':y*0, 'bot':y*0, 'left':x*0, 'right':x*0}   # Use 'n' if Newmann
     
-    def run(self,iteractions=None):
-        if iteractions:           
-            for k in range(iteractions):
-                self.iterate()
-        else:
+    def run(self,maxiteractions=None):
+        this = self.space.copy()
+        while True:
+            self.iterate()
+            d = max(abs((this-self.space)).flatten())
+            if d <= self.delta or (maxiteractions != None and self.k >= maxiteractions):                    
+                break
             this = self.space.copy()
-            while True:
-                self.iterate()
-                d = max(abs((this-self.space)).flatten())
-                if d<self.delta:                    
-                    break
-                this = self.space.copy()
+        if self.verb:
             print "Converged in %d iteractions" % self.k
             
     def bound(self,i,b):
@@ -41,7 +38,7 @@ class MDF:
             elif b == 'bot':
                 return self.space[i,self.space.shape[1]-1]
             elif b == 'left':
-                return self.space[1,i]
+                return self.space[1,i]  
             elif b == 'right':
                 return self.space[self.space.shape[0]-1,i]
         return r
@@ -51,6 +48,7 @@ class MDF:
         sy = range(self.space.shape[1])
         for ix in sx:
             for iy in sy:
+                v1 = self.space[ix,iy]
                 vt = self.space[ix,iy-1] if iy > min(sy) else self.bound(ix,
                     'top')
                 vb = self.space[ix,iy+1] if iy < max(sy) else self.bound(ix,
@@ -59,39 +57,12 @@ class MDF:
                     'left')
                 vr = self.space[ix+1,iy] if ix < max(sx) else self.bound(iy,
                     'right')
-                #v = v1 + (self.alpha/4.0) * ( ((vt+vb+vl+vr)/4.0) - 4.0*v1 )
-                v = (vt+vb+vl+vr)/4.0   # Calculate Vk+1
+                v = (1-self.alpha)*v1 + self.alpha*((vt+vb+vl+vr)/4.0)
+                #v = (vt+vb+vl+vr)/4.0   # Calculate Vk+1
                 self.space[ix,iy] = v
         self.k += 1
                 
     def plot(self):        
-        h = plt.contour(np.rot90(self.space,1))
+        h = plt.contour(self.X, self.Y, np.rot90(self.space,1))
         plt.clabel(h, inline=1, fontsize=10)
         
-plt.subplot(2,2,1)
-ex1 = MDF(np.arange(0,10,.5), np.arange(0,10,.5))
-ex1.bounds['left'] = ['n' for a in ex1.bounds['left']]
-ex1.bounds['right'] = ['n' for a in ex1.bounds['right']]
-ex1.bounds['top'] += 100
-ex1.run()
-ex1.plot()
-plt.title("a)")
-
-plt.subplot(2,2,2)
-ex2 = MDF(np.arange(0,10,.5), np.arange(0,10,.5))
-ex2.bounds['top'] += 100
-ex2.bounds['top'][0:1] = 0
-ex2.bounds['top'][19:20] = 0
-ex2.run()
-ex2.plot()
-plt.title("b)")
-
-plt.subplot(2,2,3)
-ex3 = MDF(np.arange(0,10,.5), np.arange(0,10,.5))
-ex3.bounds['top'][2:18] = 100
-ex3.bounds['left'][2:18] = 60
-ex3.bounds['bot'][2:18] = 40
-ex3.bounds['right'][2:18] = 20
-ex3.run()
-ex3.plot()
-plt.title("c)")
