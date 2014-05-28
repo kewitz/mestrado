63b687feffcb9bcda7b5519570d5b471ab3d232b
Leonardo Kewitz <holland9@gmail.com>
2014-05-28 17:57:07 -0300

    Método de Yee adicionado.

--- Eletromagnetismo Computacional I/FDTD.yee.py
+++ Eletromagnetismo Computacional I/FDTD.yee.py
@@ -9,7 +9,9 @@ Created on Wed May 28 11:11:30 2014
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from libs.FDTD import FDTD
+import mpl_toolkits.mplot3d.axes3d as p3
+
+from libs.FDTD import Yee
 
 def gaussFactory(fdtd):
     """Gera função excitação `f(x)` de um impulso gaussiano modulado em sin."""
@@ -19,34 +21,28 @@ def gaussFactory(fdtd):
             * np.exp(-np.power(t-2*fdtd.tc,2) / width)
 
 # Simulação sem passo mágico.
-a = FDTD()
+a = Yee()
 a.setFreq(2.4E6)
-a.dt = a.dt/(1+2e-1)
-a.dx = a.lamb/5
 a.run(gaussFactory(a))
 
-# Simulação com passo mágico.
-b = FDTD()
-b.setFreq(2.4E6)
-b.run(gaussFactory(b))
+#%% Plot
+fig = plt.figure()
+ax = p3.Axes3D(fig)
 
-# Cria plot e as primeiras linhas.
-fig, ax = plt.subplots()
-line, = ax.plot(a.xd, np.ones(a.xd.shape),'r')
-line2, = ax.plot(b.xd, np.ones(b.xd.shape), 'b')
+line1, = ax.plot(a.sd, a.et[50,:], zs=0, zdir='z')
+line2, = ax.plot(a.sd, a.ht[50,:], zs=0, zdir='y')
 
-#Configura legendas e limites.
-plt.ylim(-1.5,1.5)
-plt.xlim(a.xd.min(),a.xd.max())
-plt.xlabel(r"$x (m)$")
-plt.legend(("Not Magic", "Magic Step"))
+ax.set_ylim3d(a.et.min(), a.et.max())
+ax.set_zlim3d(a.ht.min(), a.ht.max())
 
-# Animação das linhas.
 def animate(t):
-    line.set_ydata(a.st[t,:])
-    line2.set_ydata(b.st[t,:])
-    return line,
+    line1.set_data(a.sd, a.et[t,:])
+    line1.set_3d_properties(a.et[t,:])
+    line2.set_data(a.sd, a.ht[t,:])
+    line2.set_3d_properties(a.ht[t,:])
+    return line1,
     
 # Plota animação
-ani = animation.FuncAnimation(fig, animate, range(len(a.td)), interval=20)
+#ani = animation.FuncAnimation(fig, animate, range(len(a.td)), blit=False,
+#                              interval=100)
 plt.show()
--- Eletromagnetismo Computacional I/libs/FDTD.py
+++ Eletromagnetismo Computacional I/libs/FDTD.py
@@ -30,7 +30,7 @@ class FDTD:
         self.fop = fop
         self.lamb = self.vp/fop
         self.tc = 1/fop
-        self.dx = self.lamb/40.0
+        self.dx = self.lamb/10.0
         self.dt = (self.dx)/(self.vp)
         self.tal = self.tc/(2*np.sqrt(2*np.log(2)))
     
@@ -67,3 +67,63 @@ class FDTD:
             # Condição de contorno absorvente
             self.st[k+1,-1] = self.st[k, -2]
 
+class Yee:
+    """Classe de cálculo FDTD 1D."""
+    def __init__(self, verbose = True):
+        self.vp = f(299792458)
+        self.eps = f(8.854187817E-12)
+        self.mu = f(pi * 4E-7)
+        self.sigma = f(5E-15)
+        self.verbose = verbose
+
+    def setFreq(self, fop):
+        """
+        Ajusta os parâmetros da simulação de acordo com a frequência (Hz) de
+        operação desejada.
+        """
+        # Define constantes:
+        self.fop = fop
+        self.lamb = self.vp/fop
+        self.tc = 1/fop
+        self.ds = self.lamb/10.0
+        self.dt = (self.ds)/(self.vp)
+        self.tal = self.tc/(2*np.sqrt(2*np.log(2)))
+    
+    def setLambda(self, lamb):
+        """
+        Ajusta os parâmetros da simulação de acordo com o comprimento de onda
+        (m) de operação desejado.
+        """
+        self.setFreq(self.vp/lamb)
+
+    def run(self, fx):
+        """
+        Executa o FDTD para os domínios configurados.
+        """
+        # Constantes
+        dtds = -1 * self.dt / self.ds
+        
+        # Define domínios:
+        self.sd = np.arange(0,self.lamb*10,self.ds,dtype=np.float64)
+        self.td = np.arange(0,self.dt*700,self.dt,dtype=np.float64)
+        self.et = np.zeros((len(self.td),len(self.sd)))
+        self.ht = np.zeros((len(self.td),len(self.sd)))
+        
+        if self.verbose: print "DT = %.3E\nDS = %.3E\n" \
+            "Space/Time Domain: %i/%ipoints" \
+            % (self.dt,self.ds, len(self.sd), len(self.td))
+        
+        # Iterações no domínio do tempo
+        for k, t in indexed(self.td[:-1]):
+            # Excitação            
+            self.et[k,0] = fx(t)
+            # Propagação de H no espaço
+            for i, x in indexed(self.sd[0:-1]):
+                self.ht[k,i] = (dtds/self.mu) * (self.et[k,i+1] - self.et[k,i])
+                if k > 0: self.ht[k,i] += self.ht[k-1,i]
+            # Propagação de E no espaço
+            for i, x in indexed(self.sd[1:-1],1):
+                self.et[k+1,i] = (dtds/self.eps) * (self.ht[k,i] - self.ht[k,i-1])+\
+                    self.et[k,i]
+            # Condição de contorno absorvente no campo elétrico
+            self.et[k+1,-1] = 0
